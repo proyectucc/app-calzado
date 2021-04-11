0f49467cee21036b73757b16e5825f9ce343716b
Daniel <daniel.morave@campusucc.edu.co>
2021-04-10 19:28:38 -0500

    Se crean componentes y enrutamientos

--- ProyectoCalzado/ClientApp/src/app/informes/informes.component.ts
+++ ProyectoCalzado/ClientApp/src/app/informes/informes.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-counter-component',
-  templateUrl: './counter.component.html'
+  selector: 'app-informes-component',
+  templateUrl: './informes.component.html'
 })
-export class CounterComponent {
+export class InformesComponent {
   public currentCount = 0;
 
   public incrementCounter() {
