b0cc9104865a238db688671806536549c5931b1d
Daniel <daniel.morave@campusucc.edu.co>
2021-04-10 20:36:46 -0500

    Se agregan estilos y se pule el login

--- ProyectoCalzado/ClientApp/src/app/login/login.component.ts
+++ ProyectoCalzado/ClientApp/src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
   /**
